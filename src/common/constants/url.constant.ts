325b6cc6e0a776a2b521d393e24304a8870156e2
myusernameismy <ilyas123.822@gmail.com>
2023-10-30 09:23:51 +0600

    fix upload url

--- src/common/constants/url.constant.ts
+++ src/common/constants/url.constant.ts
@@ -1,3 +1,4 @@
-export const UPLOAD_URL = (process.env.MODE = 'dev'
-  ? 'http://localhost:8080/api/'
-  : 'http://45.159.250.142:4012/api/');
+export const UPLOAD_URL =
+  process.env.MODE == 'dev'
+    ? 'http://localhost:8080/api/'
+    : 'http://45.159.250.142:4012/api/';
